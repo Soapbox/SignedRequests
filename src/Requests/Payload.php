d073e060ea13eb39c8e72e2972dcce01801477eb
Jaspaul Bola <jaspaul.b@gmail.com>
2017-04-26 14:16:36 -0400

    Add the ability to create a payload from both illuminate and guzzle requests

--- src/Requests/Payload.php
+++ src/Requests/Payload.php
@@ -2,7 +2,8 @@
 
 namespace SoapBox\SignedRequests\Requests;
 
-use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Psr7\Request as GuzzleRequest;
+use Illuminate\Http\Request as IlluminateRequest;
 
 class Payload
 {
@@ -19,18 +20,21 @@ class Payload
      * @param \GuzzleHttp\Psr7\Request $request
      *        The request to extract a payload from.
      */
-    public function __construct(Request $request)
+    public function __construct($request)
     {
         $this->request = $request;
     }
 
     /**
-     * Returns a payload with the id, method, uri, and content embedded.
+     * Returns the payload from a guzzle request.
+     *
+     * @param \GuzzleHttp\Psr7\Request $request
+     *        An instance of the guzzle request to extract a payload from.
      *
      * @return string
-     *         A json encoded payload.
+     *         The payload.
      */
-    public function __toString() : string
+    protected function generateFromGuzzleRequest(GuzzleRequest $request) : string
     {
         $id = isset($this->request->getHeader('X-SIGNED-ID')[0]) ?
             $this->request->getHeader('X-SIGNED-ID')[0] : '';
@@ -41,5 +45,45 @@ class Payload
             'uri' => (string) $this->request->getUri(),
             'content' => $this->request->getBody()
         ]);
+    }
+
+    /**
+     * Retruns the payload from an illuminate request.
+     *
+     * @param \Illuminate\Http\Request $request
+     *        An instance of the illuminate request to extract the payload from.
+     *
+     * @return string
+     *         The payload.
+     */
+    protected function generateFromIlluminateRequest(IlluminateRequest $request) : string
+    {
+        $id = $this->request->headers->get('X-SIGNED-ID', '');
+
+        return json_encode([
+            'id' => (string) $id,
+            'method' => $this->request->getMethod(),
+            'uri' => (string) $this->request->fullUrl(),
+            'content' => $this->request->getContent()
+        ]);
+    }
+
+    /**
+     * Returns a payload with the id, method, uri, and content embedded.
+     *
+     * @return string
+     *         A json encoded payload.
+     */
+    public function __toString() : string
+    {
+        if ($this->request instanceof GuzzleRequest) {
+            return $this->generateFromGuzzleRequest($this->request);
+        }
+
+        if ($this->request instanceof IlluminateRequest) {
+            return $this->generateFromIlluminateRequest($this->request);
+        }
+
+        return '';
     }
 }
--- tests/Requests/PayloadTest.php
+++ tests/Requests/PayloadTest.php
@@ -4,21 +4,22 @@ namespace Tests\Requests;
 
 use Tests\TestCase;
 use Ramsey\Uuid\Uuid;
-use GuzzleHttp\Psr7\Request;
 use SoapBox\SignedRequests\Requests\Payload;
+use GuzzleHttp\Psr7\Request as GuzzleRequest;
+use Illuminate\Http\Request as IlluminateRequest;
 
 class PayloadTest extends TestCase
 {
     /**
      * @test
      */
-    public function it_translates_a_request_to_json_encoded_string()
+    public function it_translates_a_guzzle_request_to_a_json_encoded_string()
     {
         $method = 'GET';
         $uri = 'https://localhost';
         $id = Uuid::uuid4();
 
-        $request = (new Request('GET', 'https://localhost'))
+        $request = (new GuzzleRequest('GET', 'https://localhost'))
             ->withHeader('X-SIGNED-ID', $id);
 
         $expected = json_encode([
@@ -29,5 +30,50 @@ class PayloadTest extends TestCase
         ]);
 
         $this->assertEquals($expected, (string) new Payload($request));
+    }
+
+    /**
+     * @test
+     */
+    public function it_translates_an_illuminate_request_to_a_json_encoded_string()
+    {
+        $id = (string) Uuid::uuid4();
+
+        $uri = 'https://localhost';
+        $method = 'GET';
+        $parameters = [];
+        $cookies = [];
+        $files = [];
+        $server = [
+            'HTTP_X-SIGNED-ID' => $id
+        ];
+        $content = null;
+
+        $request = IlluminateRequest::create(
+            $uri,
+            $method,
+            $parameters,
+            $cookies,
+            $files,
+            $server,
+            $content
+        );
+
+        $expected = json_encode([
+            'id' => $id,
+            'method' => $method,
+            'uri' => $uri,
+            'content' => $request->getContent()
+        ]);
+
+        $this->assertEquals($expected, (string) new Payload($request));
+    }
+
+    /**
+     * @test
+     */
+    public function it_translates_non_requests_to_an_empty_string()
+    {
+        $this->assertEquals('', (string) new Payload(null));
     }
 }
