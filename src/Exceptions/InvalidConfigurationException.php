8a6ef49f2f44fb1d663ca4b7fddcb923c1b8896e
Dave Wong <dave@soapboxhq.com>
2018-09-25 16:37:54 -0400

    use . notation check to ensure the request key exists

---
+++ src/Exceptions/InvalidConfigurationException.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace SoapBox\SignedRequests\Exceptions;
+
+use Exception;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+
+class InvalidConfigurationException extends Exception implements HttpExceptionInterface
+{
+    /**
+     * The default exception message.
+     *
+     * @var string
+     */
+    const MESSAGE = 'Failed to find Signed Requests configuration key';
+
+    /**
+     * Provides a default error message for an invalid signature.
+     *
+     * @param string $message
+     *        A customizable error message.
+     */
+    public function __construct(string $message = self::MESSAGE)
+    {
+        parent::__construct($message);
+    }
+
+    /**
+     * Returns an HTTP BAD REQUEST status code.
+     *
+     * @return int
+     *         An HTTP BAD REQUEST response status code
+     */
+    public function getStatusCode()
+    {
+        return Response::HTTP_UNPROCESSABLE_ENTITY;
+    }
+
+    /**
+     * Returns response headers.
+     *
+     * @return array
+     *         Response headers
+     */
+    public function getHeaders()
+    {
+        return [];
+    }
+}
